d257aae11e1bd31eb3299683f8ce233e5f1d5bbb
Yaroslav Matusevich <yari.matusevich@gmail.com>
2025-06-15 21:03:10 -0400

    Added function for analyzing article sentiment

--- server/sentiment_analyzer.py
+++ server/sentiment_analyzer.py
@@ -1,9 +1,12 @@
+from stock_data import Article
 from transformers import pipeline
 from transformers.pipelines import TextClassificationPipeline
 
 def get_sentiment_model():
-    model_name = "cardiffnlp/twitter-roberta-base-sentiment-latest"
-    return pipeline(model_name)
+    model_name = "ProsusAI/finbert"
+    return pipeline(model = model_name, return_all_scores = True)
 
-def analyze_sentiment(input_text: str, model: TextClassificationPipeline):
+def analyze_article_sentiment(article: Article, model: TextClassificationPipeline):
+    # filters out null, joins with space
+    input_text = " ".join(filter(None, [article.title, article.summary]))
     return model(input_text)
