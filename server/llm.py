622578e9ed3cf1e62d862884cac951dd76a35597
yarimatusevich <yari.matusevich@gmail.com>
2025-06-29 21:17:58 -0400

    Added function for cleaning LLM responses

--- server/llm.py
+++ server/llm.py
@@ -17,9 +17,11 @@ Using the instruct version as this model is tailored towards creating structured
  which will be needed when integrating the frontend with the backend.
 """
 
+import re
+import json
+
 from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenizerBase, PreTrainedModel
 from data_models import StockData
-# import torch
 
 def get_generative_ai_model() -> tuple[PreTrainedTokenizerBase, PreTrainedModel]:
     model_name = "deepseek-ai/deepseek-coder-1.3b-instruct"
@@ -37,7 +39,7 @@ def prompt_generative_ai_model(messages: list[dict], model_bundle: tuple[AutoMod
         add_generation_prompt=True
     ).to(model.device)
 
-    output = model.generate(input_ids, max_new_tokens=1000)
+    output = model.generate(input_ids, max_new_tokens=1000, temperature=0.0, do_sample=False)
 
     return tokenizer.decode(output[0][input_ids.shape[-1]:], skip_special_tokens=True)
 
@@ -77,7 +79,19 @@ def get_recommendation(stock_data: StockData, model_bundle: tuple[AutoModelForCa
 
     return prompt_generative_ai_model(messages, model_bundle)
 
-def write_llm_response_to_file(response: str):
-    print(f"*****************************{response}")
+def clean_model_response(response):
+    try:
+        structured_response = json.loads(response)
+        return json.dumps(structured_response)
+    except json.JSONDecodeError:
+        match = re.search(r'{.*}', response, re.DOTALL)
+
+    if match:
+        structured_response = json.loads(match.group(0))
+        return json.dumps(structured_response, indent=2)
+    else:
+        raise ValueError("No valid JSON found in model output")
+
+def write_llm_response_to_file(response: dict):
     with open("response.json", "w") as f:
-        f.write(response)
+        f.write(clean_model_response(response))
